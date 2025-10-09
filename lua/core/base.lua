46bf230cd36c52b3bd43307913e2d87a3a98b63d
fadedreams <fadedreams7@gmail.com>
2025-10-09 03:10:10 -0400

    oil+

--- lua/core/base.lua
+++ lua/core/base.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 vim.opt.termguicolors = true
+
---
+++ lua/plugins/oil.lua
@@ -0,0 +1,46 @@
+local desc = Utils.plugin_keymap_desc("oil")
+
+return {
+  "stevearc/oil.nvim",
+  dependencies = {
+    {"nvim-treesitter/nvim-treesitter"},
+    {"nvim-tree/nvim-web-devicons", lazy = true},
+  },
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,
+      is_always_hidden = function(name, _)
+        return vim.startswith(name, ".DS_Store")
+      end,
+    },
+    keymaps = {
+      ["<C-h>"] = false, -- Split
+      ["<C-l>"] = false, -- refresh
+      ["<C-s>"] = "actions.select_split",
+      ["<C-v>"] = "actions.select_vsplit",
+      ["<C-g>"] = "actions.refresh",
+    },
+    float = {padding = 4},
+  },
+  config = function(_, opts)
+    local oil = require("oil")
+    oil.setup(opts)
+    vim.keymap.set("n", "-", oil.open, {desc = desc("Open parent directory")})
+    vim.keymap.set("n", "<leader>oo",
+      function()
+        require("oil").open_float()
+        -- Wait until oil has opened, for a maximum of 1 second.
+        vim.wait(1000, function()
+          return require("oil").get_cursor_entry() ~= nil
+        end)
+        if require("oil").get_cursor_entry() then
+          require("oil").open_preview()
+        end
+      end
+      , {desc = desc("Open parent directory")})
+  end,
+}
